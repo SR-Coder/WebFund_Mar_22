9f85d3640ca11d37716fcba24346b5878eb7c5ba
Jim Reeder <reederje@gmail.com>
2022-03-22 14:33:14 -0700

    W!D2 Code Push

---
+++ algos/introToJS.js
@@ -0,0 +1,37 @@
+// DATA TYPES
+// numbers, strings, Bool, objects, array
+
+// VARIABLES
+// keyword | variable name | equals sign | value
+var myNum = 7;
+var myString = "Hello World";
+var isTired = true;
+
+let myArr = [4,6,8,2,1]
+let myNum2 = myArr[2]
+
+// FOR LOOPS
+// keyword | ( | define interator | condition | iterator | ) |  { code block }
+for(var i = 0; i< 5; i++){
+    console.log("hello ", i+1);
+}
+
+
+// Problem 1 from Algo day 1
+var a = 25;
+a = a - 13;
+console.log(a/2);
+    
+a = "hello";
+console.log(a + " hello");
+
+
+
+// Problem 2
+for(var i=0; i<10; i++) {
+    console.log(i);
+    i = i + 3; 
+}
+    
+console.log("outside of the loop " + i);
+
